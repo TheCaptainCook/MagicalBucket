c31e3a83853a73cceb5b8488c0abfd61298cbcf0
SHEIKH MASEM MANDAL <masem@duck.com>
2023-06-23 23:46:55 +0530

    Initial Commit

---
+++ src/main/java/thecaptaincook/magicalbucket/MagicalBucket.java
@@ -0,0 +1,17 @@
+package thecaptaincook.magicalbucket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MagicalBucket extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
